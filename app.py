acc482fbe90b75283cb0d834ed5bf2f8d56d84d4
Nico Mendoza <nicomendoza274@gmail.com>
2021-11-18 00:43:07 -0300

    Implementacion para sacar los promedio en beta

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from schemas.nirMeasure import NirMeasure
 import httpx
 
 app = FastAPI(
-    title="Gluco Backend",
+    title="Gluco Backend API",
     description="Backend para Gluco, aplicacion desarrollada para el proyecto de grado",
-    version="1.0.0",
+    version="1.0.1",
     openapi_tags=[{
         "name": "users",
         "description": "Rutas de usuarios"
@@ -24,6 +24,30 @@ async def request():
         result = result.json()
         return result
 
-@app.get("/medir", response_model=NirMeasure)
+@app.get("/medir")
 async def medir_nir():
-  return await request()
+    jsonNir = await request()
+    R=0
+    S=0
+    T=0
+    U=0
+    V=0
+    W=0
+    
+    for i in jsonNir:
+        print(i)
+        R += i['R']
+        S += i['S']
+        T += i['T']
+        U += i['U']
+        V += i['V']
+        W += i['W']
+    
+    promR=R/7
+    promS=S/7
+    promT=T/7
+    promU=U/7
+    promV=V/7
+    promW=W/7
+
+    return { "promR": promR, "promS":promS, "promT": promT, "promU": promU, "promV": promV, "promW": promW }
